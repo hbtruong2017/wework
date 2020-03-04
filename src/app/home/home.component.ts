f4463d5664827b3660b2791ad02ec21fcfa1d1b0
haibang_truong <haibang97@yahoo.com>
2020-03-04 21:29:55 +0800

    Add home function to cater for links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +9,28 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   maleForm: FormGroup;
+  link: string;
+  hasLink: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    window.sessionStorage.clear();
+
+    // Check if there are any query params
+    let encodedLink = ''
+    if (this.route.snapshot.queryParams['link']) {
+      this.hasLink = true;
+      this.route.queryParams.subscribe(params => {
+        encodedLink = params['link']
+      })
+      console.log(encodedLink)
+      this.link = atob(encodedLink)
+      console.log(this.link)
+      window.sessionStorage.setItem("friendInfo", this.link)
+    } 
+      
+    // Initialize new form
     this.maleForm = this.formBuilder.group({
       name: [''],
       age: [''],
@@ -32,6 +50,6 @@ export class HomeComponent implements OnInit {
     }
     console.log(maleInfo)
     window.sessionStorage.setItem("maleInfo", JSON.stringify(maleInfo))
-    this.router.navigate(["../link"])
+    this.hasLink ? this.router.navigate(["../result"]) : this.router.navigate(["../link"]) 
   }
 }
