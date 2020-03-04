8b3b04c641dee1ab3a7dcf93e5096dc4ac5a40a9
haibang_truong <haibang97@yahoo.com>
2020-03-04 20:00:42 +0800

    Add main home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,48 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
-import { DataService } from '../service/data.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-/**
- * The home component to show home page after log in
- */
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  pendingLoanList: any;
-  myName: string;
-  myBalance: any;
-  myAccountNo: any;
+  maleForm: FormGroup;
 
-  constructor(private dataService: DataService, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
-    this.myName = window.sessionStorage.name;
-    this.myBalance = window.sessionStorage.balance;
-    this.myAccountNo = window.sessionStorage.accountID;
-
-    this.dataService.getAllPendingLoans().subscribe((data:any) => {
-      console.log(data)
-      this.pendingLoanList = data.allLoans.reverse();
-      console.log(this.pendingLoanList)
-    }, error => {
-      console.log(error)
+    this.maleForm = this.formBuilder.group({
+      name: [''],
+      age: [''],
+      time: [''],
+      preferences: [''],
+      occasion: ['']
     })
   }
 
-/**
- * Function to redirect to loan detail page
- */
-  goToLoan(id: number) {
-    this.router.navigate(['/loan'], { queryParams: { id: id } })
-  }
-
-  /**
- * Function to redirect to tBank
- */
-  gotoTbank() {
-    window.location.href = "http://tbankonline.com/SMUtBank_RIB2/#/login?redirect=%2F";
+  getMaleInfo() {
+    let maleInfo = {
+      name: this.maleForm.get("name").value,
+      age: this.maleForm.get("age").value,
+      time: this.maleForm.get("time").value,
+      preferences: this.maleForm.get("preferences").value,
+      occasion: this.maleForm.get("occasion").value
+    }
+    console.log(maleInfo)
+    window.sessionStorage.setItem("maleInfo", JSON.stringify(maleInfo))
+    this.router.navigate(["../link"])
   }
 }
