fb913acc2492b3a9ca060ddb5c8c1b2ca36d28a3
haibang_truong <haibang97@yahoo.com>
2020-03-06 00:58:48 +0800

    Call unsplash API

--- src/app/friend/friend.component.ts
+++ src/app/friend/friend.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import Unsplash from 'unsplash-js';
 
 @Component({
   selector: 'app-friend',
@@ -15,10 +16,16 @@ export class FriendComponent implements OnInit {
     let encodedLink = ''
     this.route.queryParams.subscribe(params => {
       encodedLink = params['link']
+  callAPI(keyword: string) {
+    let unsplash = new Unsplash({ accessKey: "gtQitnFbwx0e36pya3jZUcE3uGbFUaOVAGey5a7IgVs" });
+
+    unsplash.search.photos(keyword, 1, 4).then((response: any) => {
+      return response.json();
+    }).then((data: any) => {
+      console.log(data)
+      this.eventPhoto = data.results[0].urls.full;
+      console.log(this.eventPhoto)
     })
-    console.log(encodedLink)
-    this.link = atob(encodedLink)
-    console.log(this.link)
   }
 
 }
